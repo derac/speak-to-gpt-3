8351259d28ee72ac4539e4f20549a5d15cb81097
derac <dwolson123@gmail.com>
2021-07-23 13:25:36 -0500

    change bot to socrates, pull out variables from f strings

--- Src/app.py
+++ Src/app.py
@@ -15,32 +15,36 @@ tts_engine.setProperty("voice", voices[0].id)
 human_words = []
 ai_words = []
 chat_history = ""
+bot_explanation = "The following is a conversation with Socrates. Socrates was a Greek philosopher from Athens who is credited as a founder of Western philosophy and the first moral philosopher of the Western ethical tradition of thought."
+bot_name = "Socrates"
+start_prompt = f"{bot_explanation}\n\nHuman: Hello, who are you?\n{bot_name}: I am Socrates. You can ask me anything.\n"
 
 with sr.Microphone() as source:
     r.adjust_for_ambient_noise(source)
-    print("Start talking to your computer now...\n")
+    print(start_prompt)
     while True:
         try:
             audio = r.listen(source)
             words = r.recognize_google(audio)
-            print(f"You: {words}")
+            print(f"Human: {words}")
             human_words.append(words)
             response = openai.Completion.create(
                 engine="davinci",
-                prompt=f"The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\n{chat_history}Human: {words}\nAI:",
+                prompt=f"{start_prompt}{chat_history}Human: {words}\nAI:",
                 temperature=0.9,
                 max_tokens=150,
                 top_p=1,
                 frequency_penalty=0.0,
                 presence_penalty=0.6,
-                stop=["\n", " Human:", " AI:"],
+                stop=["\n", " Human:", f" {bot_name}:"],
             )
             ai_words.append(response["choices"][0]["text"])
             human_words, ai_words = human_words[-5:], ai_words[-5:]
             chat_history = "".join(
-                f"Human:{human}\nAI:{ai}\n" for human, ai in zip(human_words, ai_words)
+                f"Human:{human}\n{bot_name}:{ai}\n"
+                for human, ai in zip(human_words, ai_words)
             )
-            print(f'CPU:{response["choices"][0]["text"]}')
+            print(f'{bot_name}:{response["choices"][0]["text"]}\n')
             tts_engine.say(response["choices"][0]["text"])
             tts_engine.runAndWait()
         except:
